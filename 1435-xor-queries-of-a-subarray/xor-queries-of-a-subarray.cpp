8183b839cbeba66cae6b9b463543771cabdbb682
Rishit Rathod <115222828+RishitRathod@users.noreply.github.com>
2024-09-13 22:17:26 +0530

    Time: 1963 ms (5.03%) | Memory: 41.8 MB (72.15%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
@@ -5,9 +5,7 @@ public:
         for(int i=0; i<queries.size(); i++){
             int a=0;
             for(int j=queries[i][0]; j<=queries[i][1]; j++){
-
-                a=a^arr[j];
-               
+                a=a^arr[j];  
             }
             kk.push_back(a);
         }

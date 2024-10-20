24dd4c2675daec81d0eb880b3566e66e6fd43a59
Rishit Rathod <115222828+RishitRathod@users.noreply.github.com>
2024-10-20 10:18:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (29.48%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> s1;
+        for(char c:s){
+            if(c=='{' || c=='(' || c=='['){
+                s1.push(c);
+            }
+            else{ if( s1.empty() || (c==')' && s1.top()!='(') ||
+                     (c=='}' && s1.top()!='{') ||
+                      (c==']' && s1.top()!='[') 
+            ){
+              return false; 
+            }
+            s1.pop();
+            }
+        }
+        return s1.empty();
+        
+    }
+};
